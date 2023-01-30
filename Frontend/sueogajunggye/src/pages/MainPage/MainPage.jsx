8add5f9202c5780db977ae8c3f011a439f3c85bc
강인주 <gij452@kakao.com>
2023-01-30 13:51:27 +0900

    [FE]feature - 메인 페이지 구현 재업로드

---
+++ Frontend/sueogajunggye/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "./Title";
+import VideoBtn from "./VideoBtn";
+
+function MainPage(props) {
+    return (
+        <div>
+            <Title></Title>
+            <VideoBtn></VideoBtn>
+        </div>
+    )
+}
+
+export default MainPage;
---
+++ Frontend/sueogajunggye/src/pages/MainPage/VideoBtn.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import './Main.css';
+
+// Hovering 라이브러리 필요
+// npm install react-hover-video-player
+import HoverVideoPlayer from 'react-hover-video-player';
+
+function VideoBtn(props) {
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log('video clicked');
+    }
+
+    return (
+        <div className="wrapper">
+            <div className="video-box">
+                <div className="video">
+                <HoverVideoPlayer
+                        restartOnPaused
+                        videoSrc={[
+                            { src: '/VideoSrc/v1.webm', type: 'video/webm' },
+                            { src: 'video.mp4', type: 'video/mp4' },
+                        ]}>
+                    </HoverVideoPlayer>
+                    <form onSubmit={handleSubmit}>
+                        <button className="matching-btn" type="submit">통역사매칭서비스</button>
+                    </form>
+                </div>
+
+                <div className="video">
+                <HoverVideoPlayer
+                        restartOnPaused
+                        videoSrc={[
+                            { src: '/VideoSrc/v2.webm', type: 'video/webm' },
+                            { src: 'video.mp4', type: 'video/mp4' },
+                        ]}>
+                    </HoverVideoPlayer>
+                    <form onSubmit={handleSubmit}>
+                        <button className="matching-btn" type="submit">AI통역서비스</button>
+                    </form>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default VideoBtn;
