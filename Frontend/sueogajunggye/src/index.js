8a7e9aa4d91d00e5054578a58d4592e27a41b348
강인주 <gij452@kakao.com>
2023-02-06 09:00:34 +0900

    [FE]feature - 통역사 페이지 데이터 유지 기능 구현

--- Frontend/sueogajunggye/src/index.js
+++ Frontend/sueogajunggye/src/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import store from './redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+import './index.css';
+import App from './App';
+
+
+export let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* Provider : store가 리액트 앱 전체를 감싸주는 용도 */}
     <Provider store={store}>
-      <App />
+      {/* loading : store를 불러오는 과정 중에 보여줄 컴포넌트 */}
+      {/* persistor : 로컬스토리지에 저장할 스토어 */}
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+      {/* <Profile></Profile> */}
     </Provider>
   </React.StrictMode>
 );
