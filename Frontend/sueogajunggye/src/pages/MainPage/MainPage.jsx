ee42b94d21437bbc2613a7e02dec7da3e4fa89cf
강인주 <gij452@kakao.com>
2023-02-08 17:29:10 +0900

    [FE]feat - 메인페이지 ui 수정중

--- Frontend/sueogajunggye/src/pages/MainPage/MainPage.jsx
+++ Frontend/sueogajunggye/src/pages/MainPage/MainPage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Title from "./Title";
 import VideoBtn from "./VideoBtn";
+import mainBgImg from '../../assets/images/mainBgImg.jpg'
 
 function MainPage(props) {
     return (
         <div>
             <Title></Title>
-            <VideoBtn></VideoBtn>
+            <div className="main-bg-img">
+                {/* <img className="main-bg-img" src={mainBgImg} /> */}
+                <VideoBtn></VideoBtn>
+            </div>
         </div>
     )
 }
