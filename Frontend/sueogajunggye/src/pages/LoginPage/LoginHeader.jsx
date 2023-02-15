4b8aea73d2c5ca7fff1a88ec410e68db36b9885e
lj05117 <lj05117@naver.com>
2023-02-15 10:22:12 +0900

    [FE] Feat - 로그인페이지 로고 클릭 시 메인 이동

--- Frontend/sueogajunggye/src/pages/LoginPage/LoginHeader.jsx
+++ Frontend/sueogajunggye/src/pages/LoginPage/LoginHeader.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import './LoginPage.css'
 import TransToMainBtn from '../../components/Buttons/TransToMain'
 import Logo from '../../assets/images/logo.png'
+import { Link } from 'react-router-dom';
 
 function LoginHeader(props) {
     return (
         <div className="title-box">
-            <div className="title-header"><img src={Logo} />수어가중계</div>
+            <Link to="/">
+            <button style={{border: 'none', 'background-color':'white'}}>
+            <div className="title-header" ><img src={Logo} />수어가중계</div>
+            </button>
+            </Link>
             <TransToMainBtn></TransToMainBtn>
         </div>
     );
